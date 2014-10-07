d47eb7af386fdf56da96df63222c56e9cefed7f6
Kamil Tarnowski <kam.tarnowski@gmail.com>
2014-10-07 15:29:28 +0200

    Finish chapter 10

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_action :signed_in_user
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,13 +8,13 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      @feed_items = []
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
 
   def destroy
-    @micropost.destroy
+    @micropost.delete
     redirect_to root_url
   end
 
